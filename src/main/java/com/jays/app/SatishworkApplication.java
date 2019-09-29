fc3e49c17be45510c276400c454ae7418eeedf9b
jitudv <jitudv09@gmail.com>
2019-09-30 01:42:25 +0530

    commit msg

---
+++ src/main/java/com/jays/app/SatishworkApplication.java
@@ -0,0 +1,13 @@
+package com.jays.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatishworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatishworkApplication.class, args);
+	}
+
+}
